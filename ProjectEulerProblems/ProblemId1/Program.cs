3f49623b31adf8ff2fb5cea627c4b6da6cb5ff90
Jarosław Juranek <juranek91@gmail.com>
2022-02-15 16:21:55 +0100

    Fix: performance Project Euler - Id 1
    As you only need to give final sum of numbers you can speed things up by add numbers directly in the loop.

--- ProjectEulerProblems/ProblemId1/Program.cs
+++ ProjectEulerProblems/ProblemId1/Program.cs
@@ -1,33 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace ProblemId1
 {
-    class Program
+    internal class Program
     {
         // Multiples of 3 or 5
-        /* 
+        /*
          * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
          * Find the sum of all the multiples of 3 or 5 below 1000.
          */
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int numberToSolve = 1_000;
-            var multiples = new List<long>();
             long solution = 0;
 
             for (int i = 0; i < numberToSolve; i++)
             {
                 if (i % 3 == 0 || i % 5 == 0)
-                    multiples.Add(i);
+                    solution += i;
             }
 
-            solution = multiples.Sum();
             Console.WriteLine($"Sum of all multiples below {numberToSolve} = {solution}");
             Console.ReadKey();
-            
         }
     }
 }
