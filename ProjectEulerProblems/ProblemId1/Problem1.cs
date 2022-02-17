1db8d9bed17a2ba401ba01189318f415731ea9ad
Jarosław Juranek <juranek91@gmail.com>
2022-02-17 09:46:14 +0100

    Restructuration of file system

---
+++ ProjectEulerProblems/ProblemId1/Problem1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProblemId1
+{
+    public static class Problem1
+    {
+        // Multiples of 3 or 5
+        /*
+         * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+         * Find the sum of all the multiples of 3 or 5 below 1000.
+         */
+
+        public static void Solve()
+        {
+            int numberToSolve = 1_000;
+            long solution = 0;
+
+            for (int i = 0; i < numberToSolve; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    solution += i;
+            }
+
+            Console.WriteLine($"Sum of all multiples below {numberToSolve} = {solution}");
+            Console.ReadKey();
+        }
+    }
+}
