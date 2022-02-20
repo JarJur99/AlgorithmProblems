fd75f0cae145c8635e56a8a7145e690de51b33ae
Jarosław Juranek <juranek91@gmail.com>
2022-02-20 08:56:52 +0100

    Solution: Euler Problem - Id 4

---
+++ ProjectEulerProblems/ProblemId1/Problem4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EulerProblems
+{
+    /*
+     * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+     * Find the largest palindrome made from the product of two 3-digit numbers.
+     */
+
+    public static class Problem4
+    {
+        public static void Solve()
+        {
+            int largestPalindrome = 0;
+
+            for (int i = 999; i > 0; i--)
+            {
+                for (int j = 999; j > 0; j--)
+                {
+                    int currentNumber = i * j;
+                    int reversedNumber = ReverseNumber(currentNumber);
+                    bool isPalindrome = currentNumber - reversedNumber == 0;
+
+                    if (isPalindrome)
+                    {
+                        largestPalindrome = currentNumber;
+                        break;
+                    }
+                }
+
+                if (largestPalindrome != 0)
+                    break;
+            }
+
+            Console.WriteLine($"Largest palindrome number made from the product of two 3-digit numbers is: {largestPalindrome}");
+            Console.ReadKey();
+        }
+
+        public static int ReverseNumber(int number)
+        {
+            var numberChars = number.ToString().ToCharArray();
+            Array.Reverse(numberChars);
+            return int.Parse(String.Concat(numberChars));
+        }
+    }
+}
--- ProjectEulerProblems/ProblemId1/Program.cs
+++ ProjectEulerProblems/ProblemId1/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Problem3.solve();
+            Problem4.Solve();
         }
     }
 }
