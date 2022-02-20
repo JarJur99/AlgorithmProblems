0cb927bb53a93cdfeaa2b4b205ed7705b89a077f
Jarosław Juranek <juranek91@gmail.com>
2022-02-20 08:33:59 +0100

    Solution: temporary solution to Euler Problem - Id 3
    Note: This solution works fine, however considering such large number it have major problem with both time and space complexity.

---
+++ ProjectEulerProblems/ProblemId1/Problem3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblems
+{
+    public static class Problem3
+    {
+        public static void solve()
+        {
+            var range = 600851475143;
+            var primes = GetPrimesInRange(range);
+            var dividers = new List<int>();
+            foreach (var prime in primes)
+            {
+                if (range % prime == 0)
+                    dividers.Add(prime);
+            }
+            var largestPrimeFactor = dividers[dividers.Count - 1];
+
+            Console.WriteLine($"Largest prime factor of the number: {range} is {largestPrimeFactor}");
+            Console.ReadKey();
+        }
+
+        public static List<int> GetPrimesInRange(long range)
+        {
+            var isPrimeIndicators = new List<bool>();
+            for (int i = 0; i < range; i++)
+            {
+                isPrimeIndicators.Add(true);
+            }
+            var primes = new List<int>();
+
+            for (int i = 2; i < (int)Math.Sqrt(range); i++)
+            {
+                if (isPrimeIndicators[i])
+                    for (int j = 2 * i; j < range; j += i)
+                        isPrimeIndicators[j] = false;
+            }
+
+            for (int i = 2; i < isPrimeIndicators.Count; i++)
+            {
+                if (isPrimeIndicators[i])
+                    primes.Add(i);
+            }
+
+            return primes;
+        }
+    }
+}
--- ProjectEulerProblems/ProblemId1/Program.cs
+++ ProjectEulerProblems/ProblemId1/Program.cs
@@ -1,19 +1,10 @@
-﻿using EulerProblems;
-using System;
-
-namespace ProblemId1
+﻿namespace EulerProblems
 {
     internal class Program
     {
-        // Multiples of 3 or 5
-        /*
-         * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-         * Find the sum of all the multiples of 3 or 5 below 1000.
-         */
-
         private static void Main(string[] args)
         {
-            Problem2.solve();
+            Problem3.solve();
         }
     }
 }
