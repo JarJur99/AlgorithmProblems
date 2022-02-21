50f997a03d9f86fcf6197b0d3004a7d3f1d5e10a
Jarosław Juranek <juranek91@gmail.com>
2022-02-21 12:14:10 +0100

    Fix: change in method names

--- ProjectEulerProblems/ProblemId1/Problem2.cs
+++ ProjectEulerProblems/ProblemId1/Problem2.cs
@@ -11,7 +11,7 @@ namespace EulerProblems
          * 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
          * By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
          */
-        public static void solve()
+        public static void Solve()
         {
             var fibbonacciNumbers = new List<long>();
             fibbonacciNumbers.Add(1);
--- ProjectEulerProblems/ProblemId1/Problem3.cs
+++ ProjectEulerProblems/ProblemId1/Problem3.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EulerProblems
 {
     public static class Problem3
     {
-        public static void solve()
+        /*
+         * The prime factors of 13195 are 5, 7, 13 and 29.
+         * What is the largest prime factor of the number 600851475143 ?
+         * */
+
+        public static void Solve()
         {
             var range = 600851475143;
             var primes = GetPrimesInRange(range);
