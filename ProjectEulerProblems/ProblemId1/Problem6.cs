2d1ea2bd15a1885519d54bbee754a3b9a1ca7235
Jarosław Juranek <juranek91@gmail.com>
2022-02-21 11:04:47 +0100

    Solution: Euler Problem - Id 6

---
+++ ProjectEulerProblems/ProblemId1/Problem6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EulerProblems
+{
+    public static class Problem6
+    {
+        /*
+         * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+         */
+
+        public static void Solve()
+        {
+            int range = 100;
+
+            long sumOfSquares = 0;
+            long squareofSum = 0;
+            long diff;
+
+            for (int i = 1; i <= range; i++)
+            {
+                sumOfSquares += (i * i);
+                squareofSum += i;
+            }
+
+            squareofSum *= squareofSum;
+            diff = squareofSum - sumOfSquares;
+
+            Console.WriteLine(diff);
+            Console.ReadKey();
+        }
+    }
+}
--- ProjectEulerProblems/ProblemId1/Program.cs
+++ ProjectEulerProblems/ProblemId1/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            Problem5.Solve();
+            Problem6.Solve();
         }
     }
 }
